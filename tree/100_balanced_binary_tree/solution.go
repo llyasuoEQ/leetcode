413f154cdf82cdafe47e924915af0cd16d581aae
梁亮亮 <liangliangliang@luojilab.com>
2021-04-07 23:36:16 +0800

    add leecode 100

---
+++ tree/100_balanced_binary_tree/solution.go
@@ -0,0 +1,37 @@
+package balanced_binary_tree
+
+import (
+	"math"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	lHeight := height(root.Left)
+	rHeight := height(root.Right)
+
+	if math.Abs(float64(lHeight-rHeight)) < 2 && isBalanced(root.Left) && isBalanced(root.Right) {
+		return true
+	}
+	return false
+}
+
+func height(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	lHeight := height(root.Left) + 1
+	rHeight := height(root.Right) + 1
+
+	if lHeight > rHeight {
+		return lHeight
+	}
+	return rHeight
+}
