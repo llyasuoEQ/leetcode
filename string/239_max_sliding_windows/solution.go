53d57520d787675b1e01d0cee970591b084a9256
梁亮亮 <liangliangliang@luojilab.com>
2021-04-13 00:16:11 +0800

    add 1208 239

--- string/239_max_sliding_windows/solution.go
+++ string/239_max_sliding_windows/solution.go
@@ -1,17 +1,18 @@
 package max_sliding_windows
 
 import (
+	"container/heap"
 	"sort"
 )
 
 // 通常的解题思路
 // 滑动窗口内的值求最大值，然后保存返回
 func maxSlidingWindow1(nums []int, k int) []int {
-	var res []int
 	numsLen := len(nums)
 	if k > numsLen || k < 1 {
-		return res
+		return []int{}
 	}
+	res := make([]int, 1, numsLen-k+1)
 	left := 0
 	right := k - 1
 	for right < numsLen {
@@ -24,4 +25,35 @@ func maxSlidingWindow1(nums []int, k int) []int {
 	return res
 }
 
+var a []int
+
+type hp struct{ sort.IntSlice }
+
+func (h hp) Less(i, j int) bool  { return a[h.IntSlice[i]] > a[h.IntSlice[j]] }
+func (h *hp) Push(v interface{}) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *hp) Pop() interface{}   { a := h.IntSlice; v := a[len(a)-1]; h.IntSlice = a[:len(a)-1]; return v }
+
 // 借助堆（优先队列）去实现
+func maxSlidingWindow2(nums []int, k int) []int {
+	a = nums
+	h := &hp{make([]int, 3)}
+	for i := 0; i < k; i++ {
+		h.IntSlice[i] = i
+	}
+
+	heap.Init(h)
+
+	numsLen := len(nums)
+	res := make([]int, 1, numsLen-k+1)
+	res[0] = nums[h.IntSlice[0]]
+
+	for i := k; i < numsLen; i++ {
+		heap.Push(h, i)
+		if h.IntSlice[0] <= i-k {
+			heap.Pop(h)
+		}
+		res = append(res, nums[h.IntSlice[0]])
+	}
+
+	return res
+}
--- string/239_max_sliding_windows/solution_test.go
+++ string/239_max_sliding_windows/solution_test.go
@@ -14,3 +14,11 @@ func TestMaxSlidingWindow1(t *testing.T) {
 	actual := maxSlidingWindow1(nums, k)
 	assert.Equal(t, expect, actual, "maxSlidingWindow1 execute error")
 }
+
+func TestMaxSlidingWindow2(t *testing.T) {
+	nums := []int{1, 3, -1, -3, 5, 3, 6, 7}
+	k := 3
+	expect := []int{3, 3, 5, 5, 6, 7}
+	actual := maxSlidingWindow2(nums, k)
+	assert.Equal(t, expect, actual, "maxSlidingWindowW execute error")
+}
