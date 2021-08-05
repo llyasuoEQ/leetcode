c2eeafbf31ba067351e350576fcc381566f141f1
梁亮亮 <liangliangliang@luojilab.com>
2021-08-05 16:14:46 +0800

    update

---
+++ tree/109_convert_sorted_list_to_binary_search_tree/solution.go
@@ -0,0 +1,16 @@
+package convert_sorted_list_to_binary_search_tree
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedListToBST(head *ListNode) *TreeNode {
+	return nil
+}
