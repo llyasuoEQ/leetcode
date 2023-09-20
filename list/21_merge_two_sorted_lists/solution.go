57ef1729396ebf0f12732cdd5738f83e3831a843
liangliangliang <liangliangliang@luojilab.com>
2023-09-21 01:02:07 +0800

    add 21 23

---
+++ list/21_merge_two_sorted_lists/solution.go
@@ -0,0 +1,11 @@
+package merge_two_sorted_lists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+
+	return nil
+}
---
+++ list/23_merge_k_sorted_lists/solution.go
@@ -0,0 +1,11 @@
+package merge_k_sorted_lists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+
+	return nil
+}
