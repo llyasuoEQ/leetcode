1a78daff61b670ea3350a46058419917a2831389
liangliangliang <liangliangliang@luojilab.com>
2023-09-22 18:05:22 +0800

    add 23

--- list/23_merge_k_sorted_lists/solution.go
+++ list/23_merge_k_sorted_lists/solution.go
@@ -1,8 +1,19 @@
 package merge_k_sorted_lists
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func (l *ListNode) String() string {
+	var res string
+	for l != nil {
+		res = res + fmt.Sprint(l.Val) + "->"
+		l = l.Next
+	}
+	return res
 }
 
 func newListNode(val int, next *ListNode) *ListNode {
@@ -21,16 +32,12 @@ func mergeKLists(lists []*ListNode) *ListNode {
 	return head.Next
 }
 
-// mergeKLists2 使用分治思路解决
-func mergeKLists2(lists []*ListNode) *ListNode {
-	return nil
-}
-
 // mergeTwoList 合并俩个升序列表
 func mergeTwoList(list1 *ListNode, list2 *ListNode) *ListNode {
 	node := &ListNode{}
 	head := node
 	for list1 != nil && list2 != nil {
+		fmt.Println("====", list1, list2)
 		if list1.Val < list2.Val {
 			node.Next = list1
 			list1 = list1.Next
@@ -47,4 +54,22 @@ func mergeTwoList(list1 *ListNode, list2 *ListNode) *ListNode {
 		node.Next = list1
 	}
 	return head.Next
+}
+
+// mergeKLists2 使用分治思路解决
+func mergeKLists2(lists []*ListNode) *ListNode {
+	if len(lists) < 1 {
+		return nil
+	}
+	return mergeBacktrack(lists, 0, len(lists)-1)
+}
+
+func mergeBacktrack(lists []*ListNode, left, right int) *ListNode {
+	if right-left < 1 {
+		return lists[left]
+	}
+	mid := (left + right) / 2
+	list1 := mergeBacktrack(lists, 0, mid)
+	list2 := mergeBacktrack(lists, mid+1, right)
+	return mergeTwoList(list1, list2)
 }
--- list/23_merge_k_sorted_lists/solution_test.go
+++ list/23_merge_k_sorted_lists/solution_test.go
@@ -15,6 +15,16 @@ func TestMergeKLists(t *testing.T) {
 	expect := newListNode(1, newListNode(1, newListNode(2,
 		newListNode(3, newListNode(4, newListNode(4,
 			newListNode(5, newListNode(6, nil))))))))
-	actual := mergeKLists(lists)
-	assert.Equal(t, expect, actual, "mergeKLists execute error")
+	actual1 := mergeKLists(lists)
+
+	// 测试第二种方法
+	list1 = newListNode(1, newListNode(4, newListNode(5, nil)))
+	list2 = newListNode(1, newListNode(3, newListNode(4, nil)))
+	list3 = newListNode(2, newListNode(6, nil))
+	var list4 *ListNode
+	var lists2 []*ListNode
+	lists2 = append(lists2, list1, list2, list3, list4)
+	actual2 := mergeKLists2(lists2)
+	assert.Equal(t, expect, actual1, "mergeKLists execute error")
+	assert.Equal(t, expect, actual2, "mergeKLists2 execute error")
 }
