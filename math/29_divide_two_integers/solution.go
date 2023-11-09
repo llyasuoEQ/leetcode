f755d8d7581893e2f4157e82fe5d719b6b112562
liangliangliang <liangliangliang@luojilab.com>
2023-11-09 15:31:51 +0800

    add 29

--- math/29_divide_two_integers/solution.go
+++ math/29_divide_two_integers/solution.go
@@ -1,6 +1,77 @@
 package divide_two_integers
 
-func divide(dividend int, divisor int) int {
+import "math"
 
-	return 0
+// divide 先计算结果的正负，然后用被除数依次减去除数
+// 这个解法当被除数较大除数较小的时候会超出时间限制
+func divide(dividend int, divisor int) int {
+	if dividend == math.MinInt32 && divisor == -1 {
+		return math.MaxInt32
+	}
+
+	// dividend，divisor 同号为正，不同号为负
+	sign := 1
+	if (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) {
+		sign = -1
+	}
+
+	// dividend和divisor分别取绝对值，依次对dividend减去divisor，
+	// 直到dividend < divisor，返回截取的次数 * sign
+	var (
+		result      = 0
+		absDividend = abs(dividend)
+		absDivisor  = abs(divisor)
+	)
+
+	for absDividend >= absDivisor {
+		absDividend -= absDivisor
+		result++
+	}
+	if sign < 0 {
+		result = -result
+	}
+	return result
+}
+
+// divide2 先计算结果的正负，然后用被除数依次减去除数
+func divide2(dividend int, divisor int) int {
+	if dividend == math.MinInt32 && divisor == -1 {
+		return math.MaxInt32
+	}
+	// dividend，divisor 同号为正，不同号为负
+	sign := 1
+	if (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) {
+		sign = -1
+	}
+
+	// dividend和divisor分别取绝对值，依次对dividend减去divisor，
+	// 直到dividend < divisor，返回截取的次数 * sign
+	var (
+		result      = 0
+		absDividend = abs(dividend)
+		absDivisor  = abs(divisor)
+	)
+
+	// 这里用二分查找法，不然会超出时间限制
+	for absDividend >= absDivisor {
+		temp, multiple := absDivisor, 1
+		for absDividend >= (temp << 1) {
+			temp = temp << 1
+			multiple = multiple << 1
+		}
+		absDividend -= temp
+		result += multiple
+	}
+	if sign < 0 {
+		result = -result
+	}
+	return result
+}
+
+// abs 取绝对值
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
--- math/29_divide_two_integers/solution_test.go
+++ math/29_divide_two_integers/solution_test.go
@@ -1,17 +1,45 @@
 package divide_two_integers
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type TestCase struct {
+	Dividend int
+	Divisor  int
+	Expected int
+}
+
 func TestDivide(t *testing.T) {
-	var (
-		dividend = 10
-		divisor  = 3
-	)
-	expect := 3
-	actual := divide(dividend, divisor)
-	assert.Equal(t, expect, actual, "divide execute error")
+	testCases := []TestCase{
+		{
+			Dividend: 10,
+			Divisor:  3,
+			Expected: 3,
+		},
+		{
+			Dividend: 7,
+			Divisor:  -3,
+			Expected: -2,
+		},
+		{
+			Dividend: 2147483647,
+			Divisor:  3,
+			Expected: 715827882,
+		},
+		{
+			Dividend: -2147483648,
+			Divisor:  -1,
+			Expected: 2147483647,
+		},
+	}
+	for _, c := range testCases {
+		actual := divide(c.Dividend, c.Divisor)
+		assert.Equal(t, c.Expected, actual, fmt.Sprintf("divide execute error: dividend[%d], divisor[%d]", c.Dividend, c.Divisor))
+		actual = divide2(c.Dividend, c.Divisor)
+		assert.Equal(t, c.Expected, actual, fmt.Sprintf("divide2 execute error: dividend[%d], divisor[%d]", c.Dividend, c.Divisor))
+	}
 }
