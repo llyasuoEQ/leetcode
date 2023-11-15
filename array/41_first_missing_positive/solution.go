1fe8a9972d5dbc6c6887d137e80a8c1fb4960de3
liangliangliang <liangliangliang@luojilab.com>
2023-11-15 17:34:58 +0800

    add 41

--- array/41_first_missing_positive/solution.go
+++ array/41_first_missing_positive/solution.go
@@ -1,7 +1,23 @@
 package first_missing_positive
 
 // firstMissingPositive 缺失的第一个正整数
+// 思路：利用桶排序思路，一个萝卜一个坑，这里的萝卜就是数组元素
+// 坑就是数组的下标，将数组元素放置再对应的下标位置，最后遍历不
+// 等于下标的元素就是确实的第一个正整数
 func firstMissingPositive(nums []int) int {
-	// 1.
-	return 0
+	numsLen := len(nums)
+	// 1. 将各个元素放置对应下标的位置
+	for i := 0; i < numsLen; i++ {
+		for nums[i] > 0 && nums[i] <= numsLen && nums[nums[i]-1] != nums[i] {
+			nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+		}
+	}
+
+	// 2. 找出数组元素不等于下表的元素即为缺失的第一个正整数
+	for i, num := range nums {
+		if num != i+1 {
+			return i + 1
+		}
+	}
+	return numsLen + 1
 }
