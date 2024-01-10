e1fc9f5fe84c05bf73a003fdc568a2efa98b2070
liangliangliang <liangliangliang@luojilab.com>
2024-01-10 23:56:21 +0800

    add

---
+++ array/46_primutations/solution_test.go
@@ -0,0 +1,43 @@
+package primutations
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPermute(t *testing.T) {
+	testCases := []struct {
+		Nums     []int
+		Expected [][]int
+	}{
+		{
+			Nums: []int{1, 2, 3},
+			Expected: [][]int{
+				{
+					1, 2, 3,
+				},
+				{
+					1, 3, 2,
+				},
+				{
+					2, 1, 3,
+				},
+				{
+					2, 3, 1,
+				},
+				{
+					3, 1, 2,
+				},
+				{
+					3, 2, 1,
+				},
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		actual := permute(testCase.Nums)
+		assert.Equal(t, testCase.Expected, actual,
+			"trap2 execute error: Nums[%v], actual[%d], expected[%d]", testCase.Nums, actual, testCase.Expected)
+	}
+}
