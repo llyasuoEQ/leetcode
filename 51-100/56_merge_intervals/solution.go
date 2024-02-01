d43e257a6606c104ed0054b95caef0bdead6c1da
liangliangliang <liangll0921@163.com>
2024-02-01 17:10:22 +0800

    add 55

--- 51-100/56_merge_intervals/solution.go
+++ 51-100/56_merge_intervals/solution.go
@@ -1,6 +1,36 @@
 package merge_intervals
 
-func merge(intervals [][]int) [][]int {
+import "sort"
 
-	return nil
+// merge 合并区间
+// 解题思路：使用排序和合并区间的方法来解答
+func merge(intervals [][]int) [][]int {
+	// 对区间起始进行排序
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	var merged [][]int
+	merged = append(merged, intervals[0])
+
+	for i := 1; i < len(intervals); i++ {
+		last := merged[len(merged)-1]
+		curr := intervals[i]
+
+		// 当前区间的起始位置在上一个合并区间范围内
+		if curr[0] <= last[1] {
+			last[1] = max(last[1], curr[1])
+		} else {
+			merged = append(merged, curr)
+		}
+	}
+
+	return merged
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
---
+++ 51-100/56_merge_intervals/solution_test.go
@@ -0,0 +1,49 @@
+package merge_intervals
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMerge(t *testing.T) {
+
+	testCases := []struct {
+		Intervals [][]int
+		Expected  [][]int
+	}{
+		{
+			Intervals: [][]int{
+				{
+					1, 3,
+				},
+				{
+					2, 6,
+				},
+				{
+					8, 10,
+				},
+				{
+					15, 18,
+				},
+			},
+			Expected: [][]int{
+				{
+					1, 6,
+				},
+				{
+					8, 10,
+				},
+				{
+					15, 18,
+				},
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		actual := merge(testCase.Intervals)
+		assert.Equal(t, testCase.Expected, actual, fmt.Sprintf("canJump execute error: intervals[%v], expected[%v], actual[%v]",
+			testCase.Intervals, testCase.Expected, actual))
+	}
+}
